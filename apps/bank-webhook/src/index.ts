ae207b9eac8f2ee187b6c9154c1a29f412607b99
swastikCommits <textswastik.alt@gmail.com>
2025-07-17 11:37:54 +0530

    created the bank-webhook and its schema

---
+++ apps/bank-webhook/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import db from "@repo/db/client"
+const app = express();
+
+app.post("/hdfcWebhook", (req, res) => {
+    
+    const paymentInformation = {
+        token: req.body.token,
+        userId: req.body.user_identifier,
+        amount: req.body.amount
+    };
+
+    
+    
+})
